eae8d3eb2aff19d0b3ca3f3a186f6dc86bdceaf5
zummait13 <34415872+zummait13@users.noreply.github.com>
2024-08-14 10:05:49 +0300

    Fix a bug with wrapping and make container flex

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-// h1 text
-
-// grid input x input with fix width
-// max input is 100
-// by "for" method to create div elements
-// flex-wrip
-// each div has border
 // when coursor hovers div changes background color
 // when hovers again opacity +10%
 
 // slider for number of grid
 // two modes - buttons - colorful and black-white
 
-// get container width and numbers input
-// divide it and assign it to div element
-
 const gridContainer = document.querySelector(".gridContainer");
 const inputGridNumber = document.querySelector("#gridNumber");
 
 function buildGrid() {
 
-let gridSize = (gridContainer.offsetWidth / +inputGridNumber.value);
+let gridSizePercent = (gridContainer.offsetWidth / +inputGridNumber.value) / gridContainer.offsetWidth *100;
 
 for (let i = 1; i <= inputGridNumber.value**2; i++) {
     let cell = document.createElement("div");
-    cell.style.boxSizing = "border-box";
-    cell.style.width = `${gridSize}px`;
+    cell.style.width = `${gridSizePercent}%`;
+    // cell.textContent = `${i}`;
+    // cell.style.fontSize = "10px";
     gridContainer.appendChild(cell).className = "gridCell";
 }
 
